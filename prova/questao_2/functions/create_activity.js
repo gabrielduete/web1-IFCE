a1c7f475709505ba9b43a497e29105696aa99e61
Paula Micheli <paula.michele.581@gmail.com>
2022-10-10 22:17:23 -0300

    :sparkles:feat(): Adicionando script js

---
+++ prova/questao_2/functions/create_activity.js
@@ -0,0 +1,6 @@
+function createActivity(title, description){
+    
+
+}
+
+export default createActivity;
---
+++ prova/questao_2/functions/new_activity.js
@@ -0,0 +1,76 @@
+let inputTitleActivity = document.getElementById('#title');
+let inputDescriptionActivity = document.getElementById('#description');
+let btnAddActivity = document.getElementById('#btnAddActivity');
+let listActivity = document.getElementById("#listActivity");
+
+
+inputTitleActivity.addEventListener("keypress", (e) => {
+
+    let tituloTarefa = {
+        nome: inputTitleActivity.value,
+        id: gerarID(),
+    }
+
+});
+
+
+inputDescriptionActivity.addEventListener("keypress", (e) => {
+
+    let descricaoTarefa = {
+        nome: inputDescriptionActivity.value,
+
+    }
+});
+
+
+btnAddActivity.addEventListener("click", (e) => {
+
+
+    let tituloTarefa = {
+        nome: inputTitleActivity.value,
+        id: gerarID(),
+    }
+
+    let descricaoTarefa = {
+        nome: inputDescriptionActivity.value,
+    }
+
+    console.log(tituloTarefa.id, descricaoTarefa.value);
+
+    addNewActivity(tituloTarefa, descricaoTarefa);
+});
+
+
+function gerarID() {
+    return Math.floor(Math.random() * 200);
+}
+
+function addNewActivity(tituloTarefa, descricaoTarefa) {
+    let div = createTagDiv(tituloTarefa, descricaoTarefa);
+    listActivity.appendChild(div);
+    inputTitleActivity.value = "";
+    inputDescriptionActivity.value = "";
+
+}
+
+function createTagDiv(tituloTarefa, descricaoTarefa) {
+
+    let div = document.createElement("div");
+
+
+    let h3 = document.createElement("h3");
+    h3.classList.add("titleCard");
+    h3.innerHTML = tituloTarefa.value;
+
+
+    let h4 = document.createElement("h4");
+    h4.classList.add("descriptionCard");
+    h4.innerHTML = descricaoTarefa;
+
+
+    div.appendChild(h3);
+    div.appendChild(h4);
+
+    return div;
+
+}
